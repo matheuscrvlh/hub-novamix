a7d385f389fdf59a807ce239f39fa334ce7258a7
Matheus Carvalho <matheusscarvlh@gmail.com>
2026-07-29 22:04:55 -0300

    feat: adiciona frontend

---
+++ client/client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+
+export default function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element=''/>
+      </Routes>
+    </>
+  )
+}
